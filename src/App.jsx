2d3cc27865e4cf025c4cf2e272824dad2775d689
Adrian-rospx <adrianrosu5400@gmail.com>
2025-04-25 16:48:05 +0300

    created a background and added a header

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,10 @@
-import Header from './Header.jsx';
-import Planets from './Planets.jsx';
+import Header from "./Header";
 
 function App() {
     
-    const rockyPlanets = [
-        {id:1, name: "Venus", radius: 1000, temperature: 450},
-        {id:3, name: "Earth", radius: 3000, temperature: 280},
-        {id:2, name: "Mars", radius: 5000, temperature: 253},
-    ];
-
-    const gasPlanets = [
-        {id:4, name: "Jupiter", radius: 20000, temperature: 200},
-        {id:5, name: "Saturn", radius: 18000, temperature: 180},
-    ]
-
     return(
         <>
-            <Header/>
-            
-            <p>Rocky planets:</p>
-            <Planets planets={rockyPlanets} />
-
-            <p>Gas planets:</p>
-            <Planets planets={gasPlanets} />
+            <Header />
         </>
     );
 }
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,11 +1,14 @@
 
+export default function Header() {
 
-function Header() {
     return(
-        <header className="text-4xl">
-            <h1>My React App</h1>
-        </header>
+        <div className='flex bg-gray-950 m-5 p-5 max-w-4xl
+                        border-12 border-gray-900'>
+            <h1 className="font-black p-5
+                            text-center text-8xl text-shadow-gray-400
+                            text-shadow-lg">
+                Discover the Solar System
+            </h1>
+        </div>
     );
 }
-
-export default Header
